0d4d2be23d2f46a977c0b574da66e9c58b7ff0bb
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-04 08:18:58 +0000

    Conversion of Datatype

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,32 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "333qbc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "umang"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+/*
+
+1 => true; 0 => false
+"" => false; " " => true;
+"hitesh" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
