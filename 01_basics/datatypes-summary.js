d1f8c7c8a3149cbd4466d2dab365b0ae0cd5833b
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-05 05:26:05 +0000

    datatypesummary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -24,7 +24,7 @@ console.log(id === anotherId);
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
+    name: "Umang",
     age: 22,
 }
 
@@ -32,6 +32,6 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof myObj);
 
-// https://262.ecma-international.org/5.1/#sec-11.4.3
+// // https://262.ecma-international.org/5.1/#sec-11.4.3
