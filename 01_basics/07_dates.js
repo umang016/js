7e804d0d81df7382ead5052f36ae11f5f51e1946
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-09 06:36:38 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -22,5 +22,14 @@ let myTimeStamp = Date.now()
 // console.log((Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getDay());
+
+// //`${newDate.getDate()} and the time`
+
+newDate.toLocaleString('default', {
+    weekday: "short",
+})
+// console.log(newDate);
