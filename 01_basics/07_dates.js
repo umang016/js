479685454dd946ea63ffdf019e11e9f1faf71417
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-09 05:12:00 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,26 @@
+// Dates
+
+let myDate = new Date ()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+//let myCreateDate = new Date (2023, 0, 23)
+//let myCreateDate = new Date(2023, 0,23,5,3)
+//let myCreateDate = new Date("2023-01-14")
+let myCreateDate = new Date("01-14-2021")
+//console.log(myCreateDate.toDateString());
+
+//console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getSeconds());
+
+// console.log((Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getDay());
