61022247dd92f56199a6eed6c28eccc173791f3e
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-10 07:43:49 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,4 @@
-//const tinderUser = new Object()
+// const tinderUser = new Object()
 const tinderUser = {}
 
 tinderUser.id = "123abc"
@@ -8,13 +8,13 @@ tinderUser.isLoggedIn = false
 // console.log(tinderUser);
 
 const regularUser = {
-    email: "Umang@gmail.com",
+    email: "some@gmail.com",
     fullname: {
-        userfullname:{
-            firstname:"Umang",
-            lastname: "Bhadiyadra"
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
         }
-   }
+    }
 }
 
 // console.log(regularUser.fullname.userfullname.firstname);
@@ -23,35 +23,46 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-//const obj3 = {obj1,obj2}
-// const Obj3 = Object.assign({}, obj1, obj2)
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2} 
-//console.log(obj3);
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
 const users = [
     {
         id: 1,
-        email: "u@gmail.com"
+        email: "h@gmail.com"
     },
     {
-    id: 1,
-    email: "u@gmail.com"
+        id: 1,
+        email: "h@gmail.com"
     },
     {
-    },
-    {
-    },
-    {
-    },
-    {
+        id: 1,
+        email: "h@gmail.com"
     },
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('name'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+ 
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "umang"
+}
+
+//course.courseInstructor
+
+const {courseInstructor} = course
+
+console.log(courseInstructor);
