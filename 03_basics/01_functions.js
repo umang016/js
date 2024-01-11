ad76797e60e66741c55fbba703c23f295e9aa4a6
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-11 07:17:35 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,12 +1,12 @@
 
 
 function sayMyName(){
-    console.log("u");
-    console.log("m");
-    console.log("a");
-    console.log("n");
-    console.log("g");   
-}
+//     console.log("u");
+//     console.log("m");
+//     console.log("a");
+//     console.log("n");
+//     console.log("g");   
+ }
 
 //sayMyName()
 
@@ -28,12 +28,42 @@ const result = addTwoNumbers(3, 5)
 
 
 function loginusermessage(username = " sam"){
+    // if(username === undefined)
     if(!username){
-        console.log("Please Enter the Username");
+         console.log("Please Enter the Username");
         return
     }
     return `${username} just logged in`
 }
 
 // console.log(loginusermessage("Umang"));
-console.log(loginusermessage("Uman"));
+// console.log(loginusermessage("Umang"));
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 440, 500, 600));
+
+const user = {
+    username: "umang",
+    prices: 199
+}
+
+function handObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handObject(user)
+handObject({
+    username: "sam",
+    price: 399
+})
+
+const mynewArr = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+console.log(returnSecondValue(mynewArr));
