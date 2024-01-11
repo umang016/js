f2d5fc241b94f6ed21002144fe82d8e7019efc9c
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-11 11:51:11 +0000

    sc

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,13 +4,54 @@ if(true){
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 
 }
 
 
-
- console.log(a);
+//  console.log(a);
 // console.log(b);
-// console.log(c);
+// console.log(c); 
+
+
+function one(){
+    const username = "Umang"
+
+    function two(){
+        const website = "Youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+
+// one()
+
+if (true){
+    const username = "umang"
+    if(username === "umang"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++ interesting +++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
 
