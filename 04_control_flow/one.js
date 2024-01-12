6657236b5b4daae7556ad16970c895d74091ba88
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-12 07:15:15 +0000

    on

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+//if
+const isuserloggedin = true
+const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less than 50");
+// }else{
+// console.log("greater than 50");
+// }
+
+// console.log("execute");
+// <,>, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// console.log(`user power: ${power}`);
+
+
+const balance = 1000
+
+if (balance > 500) console.log("test");
