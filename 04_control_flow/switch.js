519cc3a4d93c4037258a4d0a25b9fbe3d9885d00
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-12 10:55:10 +0000

    js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log("default case ");
+        break;
+}
