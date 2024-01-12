fd2159d5fb7cf52301d217de7b76ef120f3477f0
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-12 08:25:42 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -23,4 +23,30 @@ const temperature = 41
 
 const balance = 1000
 
-if (balance > 500) console.log("test");
+// if (balance > 500) console.log("test2"), console.log("test2"); // not use for this way
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }else if (balance < 750){
+//     console.log("less than 750");
+
+// }else if(balance < 900){
+//    console.log("less than 900")
+
+// }else{
+//     console.log("less than 1200");
+
+// }
+
+const userloggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+if(userloggedin && debitcard && 2==3){
+    console.log("Allow to buy cource");
+}
+
+if (loggedinfromgoogle || loggedinfromemail) {
+    console.log("User logged in");
+}
