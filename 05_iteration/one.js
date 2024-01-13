49e564f735719bc2b55d3b6e1006526c5574c38b
umang016 <155447073+umang016@users.noreply.github.com>
2024-01-13 04:37:34 +0000

    on

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -23,6 +23,27 @@ let myArray = ["flash", "batman", "superman"]
 console.log(myArray.length);
 for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
+    
+}
+
+
+// breack and continue
+
+// for (let index = 0; index <= 20; index++) {
+//    if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//    } 
+//    console.log(`value of i is ${index}`);
+    
+// }
+
+for (let index = 0; index <= 20; index++) {
+   if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+   } 
+   console.log(`value of i is ${index}`);
     
 }
